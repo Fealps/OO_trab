90129f48da8a4ab6d68d9155faf56cd6638713ff
williamelias <williameliasalves@gmail.com>
2017-12-07 17:02:13 -0200

    update show frequencia

--- src/MenuRelatorioFrequencia.java
+++ src/MenuRelatorioFrequencia.java
@@ -6,63 +6,61 @@ import javax.swing.*;
 public class MenuRelatorioFrequencia {
 
 	private static JFrame janela;
-	private static JTabbedPane jTPane;
+	private static JTabbedPane tabela;
 	public static String textT = null;
-	public static JComponent tPanels = null;
 	// public static JLabel j3 = null;
-	public static JPanel tab;
+	public static JPanel tabelaContent;
 	public static JLabel alunoNome = null;
 	public static JLabel alunoMatricula = null;
 	static ImageIcon icon = null;
-	private static JButton c;
-	private static JComboBox<String> alunoList;
 
 	public static void apresentarGUIExibir(Aluno a, Turma t) {
 
 		janela = new JFrame();
 		janela.setVisible(true);
-		janela.setMinimumSize(new Dimension(600, 300));
+		janela.setMinimumSize(new Dimension(400, 300));
+		janela.setMaximumSize(new Dimension(600, 300));
 		janela.setTitle("Relatório de frequência de alunos");
-		//janela.pack();
+		// janela.pack();
 
-		jTPane = new JTabbedPane();
-		jTPane.setMinimumSize(new Dimension(600, 100));
-		
-		for (int i = 0; i < t.getTurmas().size();i ++) {
-			
-			tab = new JPanel();		
-			tab.setVisible(true);
-			tab.setLayout(new GridLayout(0, 2));
-			
+		tabela = new JTabbedPane();
+		tabela.setMinimumSize(new Dimension(600, 100));
+
+		for (int i = 0; i < t.getTurmas().size(); i++) {
+
+			tabelaContent = new JPanel();
+			tabelaContent.setVisible(true);
+			tabelaContent.setLayout(new GridLayout(0, 4,5,5));
+			janela.setMinimumSize(new Dimension(400, 300));
+			tabelaContent.setMaximumSize(new Dimension(600, 100));
+
+
 			textT = t.getTurmas().get(i).getDisciplina().toString();
-
 			
-			for(int j = 0;j < t.getTurmas().get(i).getAlunos().size() ;j ++) {
-				
-				alunoNome = new JLabel();	
+			tabelaContent.add(new JLabel("Nomes"));
+			tabelaContent.add(new JLabel("Matriculas"));
+			tabelaContent.add(new JLabel("P"));
+			tabelaContent.add(new JLabel("F"));
+
+			for (int j = 0; j < t.getTurmas().get(i).getAlunos().size(); j++) {
+
+				alunoNome = new JLabel();
 				alunoNome.setText(t.getTurmas().get(i).getAlunos().get(j).getNome());
-				
+
 				alunoMatricula = new JLabel();
-				alunoNome.setText(t.getTurmas().get(i).getAlunos().get(j).getMatricula());	
-				
-				tab.add(alunoNome);
-				tab.add(alunoMatricula);
-				
-				
+				alunoNome.setText(t.getTurmas().get(i).getAlunos().get(j).getMatricula());
+
+				tabelaContent.add(alunoNome);
+				tabelaContent.add(alunoMatricula);
+
 			}
 
-			
-			jTPane.addTab(textT, null, tab, "nome da disciplina");
-			jTPane.setMnemonicAt(0, KeyEvent.VK_1);
-			
+			tabela.addTab(textT, null, tabelaContent, "nome da disciplina");
+			tabela.setMnemonicAt(0, KeyEvent.VK_1);
+
 		}
-		janela.getContentPane().add(jTPane, BorderLayout.CENTER);
-
+		janela.getContentPane().add(tabela, BorderLayout.PAGE_START);
 
 	}
 
-	private static JComponent makeTextPanel(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
